4d78165169dacfa493ff0f49b6c09de0e0d9c8ca
MG <mmoroane@hotmail.com>
2023-02-22 08:00:39 +0200

    Created student-dashboard component

--- javarevisited-angular/src/app/app.module.ts
+++ javarevisited-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentDashboardComponent
   ],
   imports: [
     BrowserModule,
